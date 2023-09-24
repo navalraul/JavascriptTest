8499fa5fb15f33141053997e7138c3fca0c04c2d
navalraul <129669942+navalraul@users.noreply.github.com>
2023-09-24 13:37:06 +0530

    updates

---
+++ test.js
@@ -0,0 +1,158 @@
+// Q1 
+
+
+
+
+// Q2
+
+// function isPal(x) {
+//     const num = x.toString();
+
+//     const num2 = num.split('').reverse().join('');
+
+//     return num === num2
+// }
+
+// const x = 121;
+// const result = isPal(x)
+// console.log(result)
+
+
+
+// Q3
+
+// function calculateSum(numbers) {
+
+//     let sum = 0;
+
+//     for(let i=0; numbers.length; i++){
+//         sum += numbers[i]
+//     }
+
+//     return sum;
+// }
+
+// const numbers = [1,2,3,4,5]
+// const sum = calculateSum(numbers)
+// console.log(sum)
+
+
+
+// Q4
+
+// function isPalindrome () {
+
+//     const 
+// }
+
+
+
+
+
+// Q5
+
+// function reverseArray(rev){
+
+//     return rev.reverse()
+// }
+
+// const array = [1,2,3,4,5]
+// const reverse = reverseArray(array)
+// console.log(reverse)
+
+
+
+
+
+// Q6
+
+// let duplicates = [1,2,3,3,4]
+
+// function removeDuplicates (duplicates){
+
+//     return duplicates.filter((item, index)=> duplicates.indexOf(item) === index)
+// }
+
+// console.log(removeDuplicates(duplicates))
+
+
+// Q7
+
+
+// let n = 5;
+
+// function factorial(n){
+//     let ans = 1;
+
+//     if(n == 0) return 1;
+
+//     for(let i = 2; i <=n; i++){
+//         ans = ans*i;
+//     }
+//     return ans;
+// }
+
+// console.log(factorial(n))
+
+
+
+// Q8
+
+// function isPrime(n){
+
+//     if( n <= 1) return false;
+
+//     for(let i = 2; i <n ; i++){
+//         if(n % i == 0) return false
+//     }
+//     return true;
+// }
+
+// isPrime(2)
+// console.log(true)
+
+
+
+
+// Q9
+
+// let n = 10;
+
+// // function factorial(n){
+// //     let ans = 1;
+
+// //     if(n == 0) return 1;
+
+// //     for(let i = 2; i <=n; i++){
+// //         ans = ans*i;
+// //     }
+// //     return ans;
+// // }
+
+// // console.log(factorial(n))
+
+
+// Q10
+
+// let num = [20,57,12,98,64,35]
+// console.log(num.sort())
+
+
+// Q11
+
+function findMissingNumber() {
+    
+}
+
+
+
+// Q15
+
+// let duplicates = "hello"
+
+// function removeDuplicates (duplicates){
+
+//     return duplicates.filter((item, index)=> duplicates.indexOf(item) === index)
+// }
+
+// console.log(removeDuplicates(duplicates))
